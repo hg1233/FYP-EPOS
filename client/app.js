f76db6dc48ca2765033306e18e3cea5f21b1fdf2
hg1233 <45510654+hg1233@users.noreply.github.com>
2024-10-09 11:11:24 +0100

    setup core file for creating app & win manager
    - setup key file and code for starting application
    - setup core structure for page/window manager

---
+++ client/app.js
@@ -0,0 +1,13 @@
+const { app } = require('electron/main'); // import electron
+
+// import & setup window manager
+const windowManager = require("./window_manager.js");
+windowManager.init();
+
+
+app.whenReady().then( () => {
+
+    // open first screen
+    console.log("Hello world.");
+
+});
---
+++ client/window_manager.js
@@ -0,0 +1,39 @@
+const { BrowserWindow } = require("electron/main");
+
+class PageManager {
+    constructor() {
+        this.available_pages = [];
+        this.default_page = null;
+        this.currently_active_page = null;
+    }
+
+    // Getters & Setters for default & active page variables
+
+    getDefaultPage() { return this.default_page; }
+    
+    setDefaultPage(page) { this.default_page = page; }
+
+    getActivePage() { return this.currently_active_page; }
+
+    setActivePage(page) { this.currently_active_page = page; }
+
+
+}
+
+class Page {
+    constructor(page_name, page_file) {
+        this.page_name = page_name;
+        this.page_file = page_file;
+    }
+}
+
+const pm = new PageManager()
+
+function init() {
+
+    // create test hello world page
+    test_page = new Page("Test", "templates/test.html");
+
+}
+
+module.exports = {init, pm}
