05e1e2d43b23676f395ebe8ed7078c2189e66053
hg1233 <45510654+hg1233@users.noreply.github.com>
2024-10-13 17:42:34 +0100

    added further test for module mngr

--- client/tests/module_manager_tests.js
+++ client/tests/module_manager_tests.js
@@ -14,11 +14,28 @@ class TestModule {
 // TODO - write tests to confirm that module manager setup works as expected
 
 describe('module manager setup tests', () => {
+    let testModule = new TestModule();
+    const moduleManager = require('../managers/module_manager.js');
     it('should run init function on registration of module', async () => {
-        let testModule = new TestModule();
-        const moduleManager = require('../managers/module_manager.js');
         moduleManager.instance.registerModule("Test Module", testModule);
         expect(testModule.isModuleInitalised).toEqual(true);
+    })
+
+    it('should list all modules registered', () => {
+        expect(moduleManager.instance.getAllModuleNames().includes("Test Module")).toEqual(true);
+    })
+
+    it('should retrieve module objects by name when queried', () => {
+        expect(moduleManager.instance.getModuleByName("Test Module")).toEqual(testModule);
+    })
+
+    it('should error when sending an event privately to a module with no defined code to handle events', () => {
+        try {
+            moduleManager.instance.sendEventPrivately("Test Module", "TEST_EVENT", "TEST_DATA")
+        } catch(error) {
+            expect(true).toEqual(true);
+        }
+            expect(false).toEqual(true);
     })
 
 
