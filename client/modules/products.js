f7f4b289258073ea91c4802af1c6137a90d529c1
hg1233 <45510654+hg1233@users.noreply.github.com>
2024-10-13 18:20:05 +0100

    create method to load products & setup some example hardcoded products

--- client/modules/products.js
+++ client/modules/products.js
@@ -2,4 +2,21 @@ class ProductsModule {
     constructor() {
         this.products = {};
     }
+
+    init() {
+        this.loadProducts();
+    }
+
+    loadProducts() {
+        // TODO - replace this with a web callout (maybe web requests / api access as separate module?)
+        const hardcodedProducts = [
+            {"id": 1, "name": "Pint of Beer", "price": 450},
+            {"id": 2, "name": "Half Pint of Beer", "price": 240},
+            {"id": 3, "name": "Pint of Water", "price": 120},
+        ]
+        hardcodedProducts.forEach(product => this.addProduct(product));
+        console.log(`Loaded hardcoded products (total: ${hardcodedProducts.length()}).`)
+    }
+
+    
 }
