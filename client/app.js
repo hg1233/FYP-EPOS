a5c7179f922268cc314b030d09b3192ab59942d7
hg1233 <45510654+hg1233@users.noreply.github.com>
2024-10-21 18:52:33 +0100

    update handling of comms between frontend & backend

--- client/app.js
+++ client/app.js
@@ -23,14 +23,20 @@ app.whenReady().then( () => {
     // open first screen
     windowManager.instance.launch();
 
-    /**setTimeout(function() {
-        windowManager.instance.showPage(windowManager.instance.getPageByName("Test2"));
-    }, 5000);**/
+    ipcMain.on('winmngr.show-page', (event, page_name) => {
+        var page = windowManager.instance.getPageByName(page_name)
+        windowManager.instance.showPage(page);
+    });
 
     // handle get products func from browser
     ipcMain.handle('products.get-all-products', async () => {
-        var productsModule = moduleManager.instance.getModuleByName('products');
-        return productsModule.getAllProducts();
+        var productsModule = moduleManager.instance.broadcastEvent('GET_ALL_PRODUCTS', null);
+        return productsModule["products"];
     });
+
+    setTimeout(function() {
+        var productsModule = moduleManager.instance.getModuleByName('products');
+        productsModule.addProduct({"id": 4, "name": "Pint of Cider", "price": 550});
+    }, 10*1000)
 
 });
