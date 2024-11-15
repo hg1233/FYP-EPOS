8de6bddf801f686a38be042cf4d35c01e2949d70
hg1233 <45510654+hg1233@users.noreply.github.com>
2024-11-15 16:43:45 +0000

    generated knexfile for storing db info

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,9 +1,18 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-    dev: {
-        client: 'sqlite3',
-        connection: {
-            filename: './dev.sqlite3',
-        },
-        useNullAsDefault: true
+
+  dev: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+        directory: './migrations'
     }
+  },
+
 }
