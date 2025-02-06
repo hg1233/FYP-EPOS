a95ee54f35c3cf6571b5d80dd1358657112edd70
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-06 19:54:17 +0000

    update to frontend product funcs
    update to updateProducts to asyncronously refresh the product list from server, rather than checking for local updates
    
    added getAllProducts function accessible on frontend
    
    removed redundant variable assignment

--- client/frontend/products_link.js
+++ client/frontend/products_link.js
@@ -1,13 +1,16 @@
 var products;
 
 async function updateProducts() {
-    var products = await window.electronAPI.products_getAllProducts();
+    var products = await window.electronAPI.products_reloadProducts();
     this.products = products;
 }
 
+async function getAllProducts() {
+    return await window.electronAPI.products_getAllProducts();
+}
+
 async function getProductByID(id) {
-    var product = await window.electronAPI.products_getProductByID(id);
-    return product;
+    return await window.electronAPI.products_getProductByID(id);
 }
 
 updateProducts();
