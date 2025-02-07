5d4cc95ecdec4ec75c0cb5ee3d8324bd7482b89c
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-07 20:16:48 +0000

    basic import of clerk data

--- client/modules/clerks.js
+++ client/modules/clerks.js
@@ -2,12 +2,12 @@ class ClerksModule {
 
     net_manager;
     current_clerk;
-    cached_clerk_data;
+    clerks;
 
     constructor() {
         this.current_clerk = null;
         this.net_manager = null;
-        this.cached_clerk_data = {};
+        this.clerks = {};
     }
 
     init() {
@@ -17,8 +17,29 @@ class ClerksModule {
     async cacheClerks() {
         var clerks = await this.net_manager.pre_ready_request('/api/clerks/get/all');
         // TODO - convert api data into local array (organised by PIN # maybe?)
+
+        clerks.forEach(clerk => this.addClerkToLocalCache(clerk))
+        console.log(`Loaded products (total: ${this.clerks.length}).`)
     }
 
+    async addClerkToLocalCache(clerk) {
+        try {
+            this.validateClerkData(clerk);
+            this.clerks[clerk.id] = clerk;
+        } catch(err) {
+            // dont add clerk if they fail validation
+            return;
+        }
+    }
+
+    validateClerkData() {
+        // TODO
+    }
+
+    async findClerkByPIN(pin) {
+        // TODO
+    }
+    
 }
 
 const instance = new ClerksModule();
