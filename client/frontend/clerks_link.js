8d87d35c725af80ee46fb7762884378bd33360d5
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-08 11:19:36 +0000

    added reload clerk func
    added reload clerk func, changed updateClerks function name to match what function does (gets all clerk data from server)

--- client/frontend/clerks_link.js
+++ client/frontend/clerks_link.js
@@ -4,20 +4,25 @@ async function getClerkByPIN(pin) {
     return await window.electronAPI.clerks_getClerkByPIN(pin);
 }
 
-async function updateClerks() {
-    clerks = await window.electronAPI.clerks_getAllClerks();
+async function getAllClerks() {
+    return await window.electronAPI.clerks_getAllClerks();
 }
 
 async function getClerkByID(id) {
     return await window.electronAPI.clerks_getClerkByID(id);
 }
 
-updateClerks();
 async function createClerk(name, pin) {
     return await window.electronAPI.clerks_createClerk({name: name, pin: pin});
 }
 
+async function reloadClerks() {
+    clerks = await window.electronAPI.clerks_reloadClerks();
+}
+
+reloadClerks();
+
 setTimeout(() => {
     // update clerks in the browser's local variable every 60 seconds
-    updateClerks();
+    reloadClerks();
 }, 60*1000);
