4136c70eee5273c06d51a6a73ea1359dfeed72b1
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-11 21:16:20 +0000

    start of work to import cats/products link

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -7,8 +7,9 @@ const db = require('../database.js');
 // setup route handler
 const router = express.Router();
 
-// import Categories db model
+// import Categories db model & products link model
 const Categories = require('../database/models/CategoriesModel.js');
+const CatsProductsLink = require('../database/models/CatsProductsLinkModel.js');
 
 router.get("/get/all", async (request, response) => {
     try {
@@ -27,6 +28,23 @@ router.get("/get/:id", async (request, response) => {
         // if category not found
         if(cat == undefined) {
             response.json({error: "Category not found"});
+            return;
+        }
+
+        response.json(cat);
+    } catch(error) {
+        console.log(error);
+        response.status(500).json({error: "Failed to retrieve category from ID provided"})
+    }
+})
+
+router.get("/get/:id/products", async (request, response) => {
+    try {
+        var cat = await CatsProductsLink.getByCatID(request.params.id);
+
+        // if category not found
+        if(cat == undefined) {
+            response.json({error: "No results found"});
             return;
         }
 
