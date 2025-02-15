5de2ae07886f9c0c7c8041727f163ef62720dde9
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-15 14:27:07 +0000

    create tables endpoint

--- server/routes/tables.js
+++ server/routes/tables.js
@@ -18,7 +18,7 @@ router.get("/get/all", async (request, response) => {
         response.status(200).json(tables);
 
     } catch(err) {
-        console.error("Error occurred retrieving all tables:", error)
+        console.error("Error occurred retrieving all tables:", err)
         response.status(500).json({error: "Error occurred retrieving all tables"})
     }
 
@@ -32,6 +32,7 @@ router.get("/get/:id", async (request, response) => {
 
         // if table not found
         if(table == undefined) {
+            console.log(`[Tables > Get By ID] No table matching requested ID '${request.params.id}' found`);
             response.json({error: "Table not found"});
             return;
         }
@@ -39,11 +40,52 @@ router.get("/get/:id", async (request, response) => {
         response.status(200).json(table);
 
     } catch(err) {
-        console.error(`Error occurred retrieving table from ID ${request.params.id}:`, error)
+        console.error(`Error occurred retrieving table from ID ${request.params.id}:`, err)
         response.status(500).json({error: "Error occurred retrieving table from ID"})
     }
 
 
 })
+
+router.post("/create", async (request, response) => {
+
+    try {
+
+        var display_name = request.body["display_name"];
+        var seats = request.body["seats"];
+
+        // input validation
+        if(!isDisplayNameValid(display_name)) {
+            console.log("[Tables > Create] Input validation failed for display_name - creation of table prevented");
+            response.status(400).json({error: "Error creating table - display name invalid"})
+            return;
+        }
+
+        // check seats input is a valid number
+        if(!isSeatsValid(seats)) {
+            console.log("[Tables > Create] Input validation failed for seats - creation of table prevented");
+            response.status(400).json({error: "Error creating table - seats input invalid"})
+            return;
+        }
+
+        var table = (await Tables.create(display_name, seats))[0]
+        console.log("[Tables > Create] New table created. ID:", table.id)
+        response.status(200).json({message: "Successfully created new table.", table_id: table.id})
+
+    } catch(err) {
+        console.error("Error occurred creating table:", err)
+        response.status(500).json({error: "Error occurred creating table"})
+    }
+})
+
+// name must not be undefined, not be blank & be longer than 0 chars
+function isDisplayNameValid(name) {
+    return name != undefined && name.trim() != "" && name.length != 0;
+}
+
+// must be a valid number when parsed
+function isSeatsValid(seats) {
+    return !isNaN(Number(seats));
+}
 
 module.exports = router
