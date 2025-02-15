04cd823d67eb76977677e16e1a26ce76c3b8244c
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-15 14:09:10 +0000

    get table by ID endpoint

--- server/routes/tables.js
+++ server/routes/tables.js
@@ -24,4 +24,26 @@ router.get("/get/all", async (request, response) => {
 
 })
 
+router.get("/get/:id", async (request, response) => {
+
+    try {
+
+        var table = await Tables.getByID(request.params.id);
+
+        // if table not found
+        if(table == undefined) {
+            response.json({error: "Table not found"});
+            return;
+        }
+
+        response.status(200).json(table);
+
+    } catch(err) {
+        console.error(`Error occurred retrieving table from ID ${request.params.id}:`, error)
+        response.status(500).json({error: "Error occurred retrieving table from ID"})
+    }
+
+
+})
+
 module.exports = router
