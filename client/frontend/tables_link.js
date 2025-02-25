89c7b0323eaad9aefdc9f4ff2e29d76d0ceace91
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-25 14:53:22 +0000

    create tables link

---
+++ client/frontend/tables_link.js
@@ -0,0 +1,21 @@
+let tables;
+
+async function getAllTables() {
+    return await window.electronAPI.tables_getAll();
+}
+
+async function getTableByID(id) {
+    return await window.electronAPI.tables_getByID(id)
+}
+
+async function createTable(name, seats) {
+  return await window.electronAPI.tables_create(name, seats)
+}
+
+async function setTableStatus(id, new_status) {
+    return await window.electronAPI.tables_changeStatus(id, new_status)
+}
+
+async function updateTable(id, name, seats) {
+    return await window.electronAPI.tables_update(id, name, seats);
+}
