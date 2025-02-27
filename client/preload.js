45dcf6291a9590b1bf15679a898c38c17dbfe0ef
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-02-27 15:07:37 +0000

    reload closed orders

--- client/preload.js
+++ client/preload.js
@@ -67,6 +67,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
   orders_getClosed: async () => ipcRenderer.invoke('orders:get-closed'),
   orders_getByID: async (id) => ipcRenderer.invoke('orders:get-by-id', id),
   orders_reloadOpen: async () => ipcRenderer.invoke('orders:reload-open'),
+  orders_reloadClosed: async () => ipcRenderer.invoke('orders:reload-closed'),
   orders_setTable: async (order_id, table_id) => ipcRenderer.invoke('orders:set-table', order_id, table_id),
 
 })
