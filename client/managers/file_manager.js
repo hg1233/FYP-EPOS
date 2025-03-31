bc1f6a9fef4966743d2893ab58a2a5b7b6f5b1f6
hg1233 <45510654+hg1233@users.noreply.github.com>
2025-03-31 19:54:15 +0100

    begin work for file manager

---
+++ client/managers/file_manager.js
@@ -0,0 +1,39 @@
+class FileManager {
+
+    user_data_dir;
+    config;
+
+    constructor() {
+        this.user_data_dir = app.getPath('userData');
+        loadLocalConfig();
+    }
+
+    async loadLocalConfig(user_data_dir) {
+
+        if(await this.checkIfLocalConfigExists(user_data_dir) == false) {
+            // TODO create local config
+        }
+
+        try {
+            var data = await fs.readFile(path.join(user_data_dir, 'epos_config.json'));
+            var json = JSON.parse(data);
+            console.debug(user_data_dir)
+
+            // TODO import server_host & api key
+            // TODO send test request (heartbeat?) to confirm connection ok
+        } catch(error) {
+            console.error("Error when reading local config", error)
+        }
+    }
+
+    async checkIfLocalConfigExists(user_data_dir) {
+        try {
+            var exists = await fs.access(path.join(user_data_dir, 'epos_config.json'))
+            return true;
+        } catch(error) {
+            return false;
+        }
+    }
+}
+
+// TODO
